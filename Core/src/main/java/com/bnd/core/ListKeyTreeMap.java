4fd5cff20f70a39f2bc212321541267bea5b7b0f
Peter Banda <tremor11@gmail.com>
2019-08-12 21:31:45 +0200

    Maven deployment plugins added

--- Core/src/main/java/com/bnd/core/ListKeyTreeMap.java
+++ Core/src/main/java/com/bnd/core/ListKeyTreeMap.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
 public class ListKeyTreeMap<K,V> implements Map<Iterable<K>,V> {
 
